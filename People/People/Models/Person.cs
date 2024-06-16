42ed1175bbb3c95aebb2e4303777ed0cb5b1f74e
Romsesito <graphicsrom@gmail.com>
2024-06-16 17:03:03 -0500

    Cambios para el correcto funcionamiento de la app

---
+++ People/People/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace People.Models;
+
+using SQLite;
+
+[Table("people")]
+public class Person
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(250), Unique]
+    public string Name { get; set; }
+}
